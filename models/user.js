8329d999020e6b5175bdc1ef747b167d518b56ad
Vincent-lebrun <lebrunvincent12@gmail.com>
2022-02-02 09:28:32 +0100

    creation of my model user

---
+++ models/user.js
@@ -0,0 +1,17 @@
+//import tools
+const mongoose = require("mongoose");
+
+//call import to create model via mongoose
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+  id: Number,
+  firstname: String,
+  lastname: String,
+  password: String,
+});
+
+//creation of model user
+const User = mongoose.model("User", UserSchema);
+
+module.exports = User;
